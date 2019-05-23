3efd9127a16f361507b410f2f8262f5affcc0edd
helloalexdodd <hello@alexdodd.ca>
2019-05-23 15:57:26 -0400

    getting api call returns

--- script/script.js
+++ script/script.js
@@ -2,36 +2,47 @@ app = {};
 
 app.key = `12566759-41f529f34c688ea00af1b8bca`;
 
-app.getResults = (query) => {
-	$.ajax({
-		url: 'https://api.myjson.com/bins/18g2uk',
+app.collectiveURL = `https://api.myjson.com/bins/18g2uk`;
+
+app.photoURL = `https://pixabay.com/api/?key=12566759-41f529f34c688ea00af1b8bca&q=elephants&image_type=photo`;
+
+app.collectiveResults = $.ajax({
+		url: app.collectiveURL,
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			format: 'json',
+			q: ``
+		}
+}).then(function (results) {
+	console.log(results)
+	app.photoResults = $.ajax({
+		url: app.photoURL,
 		method: 'GET',
 		dataType: 'json',
 		data: {
 			key: app.key,
 			format: 'json',
-			q: query
+			q: ``
 		}
 	}).then(function (results) {
 		console.log(results)
 	});
-};
+});
+
 app.displayPhoto = () => {
 
 };
 
 app.displayCollective = () => {
-// array of 
 
 };
 
 app.userInput = () => {
 
-// })
 };
 
 app.init = () => {
-	app.getResults(``);
 	// app.userInput();
 };
 $(function () {
