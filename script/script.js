f20060acfa8b6d9f097fbfa6e973b992779b155d
leungcheryl <leung.cheryl@icloud.com>
2019-05-24 09:59:30 -0400

    input functional and api's pulling correctly

--- script/script.js
+++ script/script.js
@@ -4,31 +4,46 @@ app.key = `12566759-41f529f34c688ea00af1b8bca`;
 
 app.collectiveURL = `https://api.myjson.com/bins/18g2uk`;
 
-app.photoURL = `https://pixabay.com/api/?key=12566759-41f529f34c688ea00af1b8bca&q=elephants&image_type=photo`;
+app.photoURL = `https://pixabay.com/api/?`;
+// key=12566759-41f529f34c688ea00af1b8bca&q=elephant&image_type=photo
 
-app.collectiveResults = $.ajax({
-		url: app.collectiveURL,
-		method: 'GET',
-		dataType: 'json',
-		data: {
-			format: 'json',
-			q: ``
+app.collectiveResults = (pluralAnimal, singularAnimal) => {
+	
+	$.ajax({
+	url: app.collectiveURL,
+	method: 'GET',
+	dataType: 'json',
+	data: {
+		format: 'json'
+	}
+	}).then(function (results) {
+		
+		if (results.animals[singularAnimal] === undefined) {
+			console.log(results.animals[pluralAnimal])
+		} else if (results.animals[pluralAnimal] === undefined) {
+			console.log(results.animals[singularAnimal])
+		} else {
+			// this is sketchy // try kangaroos but also purple but also injfjgdnrg - gives diff things
+			console.log('please enter valid animal')
 		}
-}).then(function (results) {
-	console.log(results)
-	app.photoResults = $.ajax({
-		url: app.photoURL,
-		method: 'GET',
-		dataType: 'json',
-		data: {
+	
+		app.photoResults = $.ajax({
+			url: app.photoURL, 
+			method: 'GET',
+			dataType: 'json',
+			data: {
 			key: app.key,
 			format: 'json',
-			q: ``
-		}
-	}).then(function (results) {
-		console.log(results)
-	});
-});
+			q: `${pluralAnimal}`
+			}
+		}).then(function (results) {
+			console.log(results.hits[1].largeImageURL);
+		})
+	})
+	
+} 
+
+
 
 app.displayPhoto = () => {
 
@@ -38,9 +53,33 @@ app.displayCollective = () => {
 
 };
 
-app.userInput = () => {
+app.userInputErrorHandle = (animalInLetters) => {
+	const lastLetter = animalInLetters.slice(-1);
+	if (lastLetter[0] === 's') {
+		const pluralAnimal = animalInLetters.join('');
+		const singular = animalInLetters.slice(0, -1);
+		const singularAnimal = singular.join('');
+		app.collectiveResults(pluralAnimal, singularAnimal);
+		
+	} else if (lastLetter[0] != 's') {
+		const singularAnimal = animalInLetters.join('');
+		const plural = animalInLetters.push('s');
+		const pluralAnimal = animalInLetters.join('');
+		app.collectiveResults(pluralAnimal, singularAnimal)
+	}
+}
 
+
+app.userInput = () => {
+	$('#submitAnimal').click(function (event) {
+    	event.preventDefault()
+		const userAnimal = $('input[id=textBox]').val();
+		const animal = userAnimal.toLowerCase();
+		const animalInLetters = animal.split('');
+		app.userInputErrorHandle(animalInLetters);
+	});
 };
+app.userInput();
 
 app.init = () => {
 	// app.userInput();
