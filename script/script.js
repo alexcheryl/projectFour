22d4b5aec5543e4e3c3e1e952e7c978e6e9cefb7
helloalexdodd <hello@alexdodd.ca>
2019-05-26 21:20:32 -0400

    working on a little more error handling

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ app = {};
 
 app.key = `12566759-41f529f34c688ea00af1b8bca`;
 
-app.collectiveURL = `https://api.myjson.com/bins/18g2uk`;
+app.collectiveURL = `https://api.myjson.com/bins/1gtczo`;
 
 app.photoURL = `https://pixabay.com/api/?`;
 
@@ -14,16 +14,16 @@ app.collectiveResults = (pluralAnimal, singularAnimal) => {
 	
 	$.ajax({
 	url: app.collectiveURL,
-	method: 'GET',
-	dataType: 'json',
+	method: `GET`,
+	dataType: `json`,
 	data: {
-		format: 'json'
+		format: `json`
 	}
 	}).then(function (results) {
-		
 		if (pluralAnimal >= 0) {
 			let randomAnimalArray = Object.entries(results.animals)
 			app.animalInput = randomAnimalArray[pluralAnimal];
+			console.log(app.animalInput)
 		} else if (results.animals[singularAnimal] !== undefined) {
 			app.animalInput = results.animals[singularAnimal]			
 			console.log(app.animalInput)
@@ -45,18 +45,17 @@ app.collectiveResults = (pluralAnimal, singularAnimal) => {
 
 			app.photoResults = $.ajax({
 				url: app.photoURL, 
-				method: 'GET',
-				dataType: 'json',
+				method: `GET`,
+				dataType: `json`,
 				data: {
 					key: app.key,
-					format: 'json',
+					format: `json`,
 					q: query,
 					orientation: `horizontal`,
 					image_type: `photo`,
 					editors_choice: true
 				}
 			}).then(function (results) {
-				// console.log(app.animalInput[0])
 				if (results.hits[0] !== undefined) {
 					app.displayBackground(results.hits[0].largeImageURL)
 					app.displayCollective(pluralAnimal, app.animalInput);
@@ -64,19 +63,17 @@ app.collectiveResults = (pluralAnimal, singularAnimal) => {
 				} else {
 					app.photoResults = $.ajax({
 					url: app.photoURL, 
-					method: 'GET',
-					dataType: 'json',
-
+					method: `GET`,
+					dataType: `json`,
 					data: {
 						key: app.key,
-						format: 'json',
+						format: `json`,
 						q: query,
 						orientation: `horizontal`,
 						image_type: `photo`,
 						editors_choice: false
 					}
 				}).then(function (results) {
-					console.log(results.hits[0])
 						if (results.hits[0] !== undefined) {
 							app.displayBackground(results.hits[0].largeImageURL)
 							app.displayCollective(pluralAnimal, app.animalInput);
@@ -97,53 +94,63 @@ app.displayBackground = (results) => {
 };
 
 app.displayCollective = (animal, results) => {
-
 	if (animal >= 0) {
 		let randomResultsString = results[1].collective;
 		let randomLowerCaseString = randomResultsString.toLowerCase();
 		$(`#instructionContainer`).html(`<p class="fact">A collection of <span class="animalText">${app.animalInput[0]}</span> is known as</p><span class="collectiveText">${randomLowerCaseString}</span>`);
 	} else {
-		console.log(results.collective)
 		let resultsString = results.collective;
 		let lowerCaseString = resultsString.toLowerCase();
 		$(`#instructionContainer`).html(`<p class="fact">A collection of <span class="animalText">${animal}</span> is known as</p><span class="collectiveText">${lowerCaseString}</span>`);
 	}
 };
 
-app.userInputErrorHandle = (animalInLetters) => {
+app.userInputErrorHandle = (animal) => {
+	// split it into individual letters
+	const animalInLetters = animal.split(``);
+	// the last three letters of the word
 	const lastLetter = animalInLetters.slice(-1);
 	const secondLast = animalInLetters.slice(-2);
-	const originalAnimal = animalInLetters.join('');
-	if (secondLast[0] === 'e'&& lastLetter[0] === 's') {
-		const pluralAnimal = animalInLetters.join('');
+	const thirdLast = animalInLetters.slice(-3);
+	let originalAnimal = animalInLetters.join(``);
+	if (lastLetter[0] === `y`) {
+		// const singular = animalInLetters.slice(-1)
+		console.log(lastLetter)
+	} else if (secondLast[0] === `e`&& lastLetter[0] === `s`) {
 		const singular = animalInLetters.slice(0, -2);
-		const singularAnimal = singular.join('');
-		app.collectiveResults(pluralAnimal, singularAnimal);
-	} else if (lastLetter[0] === 's') {
-		const pluralAnimal = animalInLetters.join('');
+		const singularAnimal = singular.join(``);
+		app.collectiveResults(originalAnimal, singularAnimal);
+	} else if (lastLetter[0] === `s`) {
 		const singular = animalInLetters.slice(0, -1);
-		const singularAnimal = singular.join('');
-		app.collectiveResults(pluralAnimal, singularAnimal);
-	} else if (secondLast[0] === 's' && lastLetter[0] === 'h') {
-		const singularAnimal = animalInLetters.join('');
-		const plural = animalInLetters.push('e', 's');
-		const pluralAnimal = animalInLetters.join('');
+		const singularAnimal = singular.join(``);
+		app.collectiveResults(originalAnimal, singularAnimal);
+	} else if (secondLast[0] === `s` && lastLetter[0] === `h`) {
+		const singularAnimal = animalInLetters.join(``);
+		console.log(singularAnimal)		
+		const pluralAnimal = animalInLetters.join(``);
+		console.log(pluralAnimal)		
 		app.collectiveResults(pluralAnimal, singularAnimal)
-	} else if (lastLetter[0] != 's') {
-		const singularAnimal = animalInLetters.join('');
-		const plural = animalInLetters.push('s');
-		const pluralAnimal = animalInLetters.join('');
+		console.log(singularAnimal)	
+	} else if (lastLetter[0] != `s`) {
+		animalInLetters.push(`s`)
+		console.log(animalInLetters)
+		const singularAnimal = animalInLetters.join(``);
+		console.log(singularAnimal)
+		const pluralAnimal = animalInLetters.join(``);
+		console.log(pluralAnimal)
 		app.collectiveResults(pluralAnimal, singularAnimal)
 	} 
 };
 
 app.userInput = () => {
-	$('#submitAnimal').click(function (event) {
-    	event.preventDefault()
-		const userAnimal = $('input[id=textBox]').val();
+	$(`#submitAnimal`).click(function (event) {
+		event.preventDefault()
+		// grab the value from the user input
+		const userAnimal = $(`input[id=textBox]`).val();
+		// make sure it's all lowercase
 		const animal = userAnimal.toLowerCase();
-		const animalInLetters = animal.split('');
-		app.userInputErrorHandle(animalInLetters);
+		// call the error handling function
+		app.userInputErrorHandle(animal);
 	});
 };
 
@@ -156,11 +163,11 @@ app.randomClick = () => {
 
 app.hideButton = () => {
 	$(`.submitContainer, .collectiveTextBox`).hide(`1000`);
-	$(`.playAgain`).show(`700`)
+	$(`.tryAgain`).show(`700`)
 }
 
 app.randomChoice = () => {
-	let random = Math.floor(Math.random() * 1000) + 1;
+	let random = Math.floor(Math.random() * 340) + 1;
 	app.collectiveResults(random)
 };
 
